ff7b409cc037631e8d50a3c7a824e6e62d61f06e
Ian Macartney <ianhmacartney@gmail.com>
2025-11-04 12:12:19 -0800

    update configs

--- example/convex/schema.ts
+++ example/convex/schema.ts
@@ -1,19 +1,17 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
-export default defineSchema(
-  {
-    users: defineTable({
-      name: v.string(),
-    }),
-    backfillCursor: defineTable({
-      creationTime: v.number(),
-      id: v.string(),
-      isDone: v.boolean(),
-    }),
-    checkboxes: defineTable({
-      idx: v.number(),
-      boxes: v.bytes(),
-    }).index("idx", ["idx"]),
-  },
-);
+export default defineSchema({
+  users: defineTable({
+    name: v.string(),
+  }),
+  backfillCursor: defineTable({
+    creationTime: v.number(),
+    id: v.string(),
+    isDone: v.boolean(),
+  }),
+  checkboxes: defineTable({
+    idx: v.number(),
+    boxes: v.bytes(),
+  }).index("idx", ["idx"]),
+});
